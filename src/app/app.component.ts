ed8b6cee72b0c1667e3969f0ddf121d3486ea183
Fabio Rizzello <fabio.rizzello.lav@gmail.com>
2018-06-06 13:03:29 +0200

    Turn around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { IRxGroupDocumentType } from './../rxdb/Group';
 import { DatabaseService } from './../rxdb/rxdb.service';
 import { Component, OnInit } from "@angular/core";
+import { RxQueryObject } from 'rxdb';
 
 @Component({
   selector: "app-root",
@@ -13,7 +15,7 @@ export class AppComponent implements OnInit {
 
   async ngOnInit() {
     const db = await this.dbService.get()
-    db.group!.find().where({title: "My Title"}) // ??
+    db.group!.find().where({title: "My Title"} as RxQueryObject<IRxGroupDocumentType>)
   }
   
 }
